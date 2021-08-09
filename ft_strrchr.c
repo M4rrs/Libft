580dacf8cd728ace4674eda15038b4c07ee6d294
M4rrs <almasnorazman2803@outlook.com>
2021-08-09 21:05:57 +0800

    fixed this shit

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -2,28 +2,18 @@
 
 char	*ft_strrchr(const char	*s, int	c)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str;
 
-	i = ft_strlen(s);
-	if (s[i] == c)
-		return ((char *)s);
-	while (s[i])
+	str = (unsigned char *)s;
+	i = ft_strlen(str);
+	if (str[i] == c)
+		return ((char *)&str[i]);
+	while (i >= 0)
 	{
-		if (s[i] == c)
-			return ((char *)s);
+		if (str[i] == c)
+			return ((char *)&str[i]);
 		i--;
 	}
 	return (NULL);
-}
-
-#include <stdio.h>
-#include <string.h>
-
-int	main()
-{
-	const	char	*a = "welcome to the bre\nad bank we sell bread we sell l9oafs. we got bread on deck, bread on the floor, TOASTED.";
-	const char c = 'd';
-
-	printf("%s\n\n", strrchr(a, c));
-	printf("%s\n", ft_strchr(a, c));
 }
