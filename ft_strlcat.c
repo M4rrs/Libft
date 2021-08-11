51fb0ad2b4be60b76f0c96af9993428951ab1255
M4rrs <almasnorazman2803@outlook.com>
2021-08-11 20:02:37 +0800

    ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -2,15 +2,19 @@
 
 size_t	ft_strlcat(char	*dst, const char	*src, size_t	dstsize)
 {
+	size_t	i;
+	size_t	j;
 
-}
-
-int main()
-{
-    char a[100] = "abcdefghijklmno";
-    char b[100] = "pqrstuvwxyz";
-
-	printf("%lu\n", ft_strlcat(a, b, 32));
-    printf("%s", a);
-    return (0);
+	i = 0;
+	j = 0;
+	while (dst[i] && i < dstsize)
+		i++;
+	while (src[j] && (i + j < dstsize - 1))
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i != dstsize)
+		dst[i + j] = '\0';
+	return (i + ft_strlen(src));
 }
