c3fa100e567b80feea4a83f4bc86f1d684e54f6d
M4rrs <almasnorazman2803@outlook.com>
2021-08-12 01:20:15 +0800

    fixed

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,1 +1,23 @@
 #include "libft.h"
+
+char	*ft_strnstr(const char	*haystack, const char	*needle, size_t	len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] && haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
