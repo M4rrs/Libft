f80fda6e67bb9a57376ce36c4d51db80b6830fc2
M4rrs <almasnorazman2803@outlook.com>
2021-08-11 21:00:04 +0800

    again

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,16 +3,12 @@
 int	ft_strncmp(const char	*s1, const char	*s2, size_t	n)
 {
 	size_t	i;
-	unsigned char *str1;
-	unsigned char *str2;
-	
-	str1 = ((unsigned char *)s1);
-	str2 = ((unsigned char *)s2);
+
 	i = 0;
-	while (i < n && (str1[i] != '\0' && str2[i] != '\0'))
+	while ((s1[i] || s2[i]) && i < n)
 	{
-		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+		if (s1[i] != s2[i])
+			return ((unsigned char *)s1[i] - (unsigned char *)s2[i]);
 		i++ ;
 	}
 	return (0);
