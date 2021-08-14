d8693ef4551fb11ef5c99ca9a0685d00eb6fc043
M4rrs <almasnorazman2803@outlook.com>
2021-08-14 12:54:30 +0800

    strdup

---
+++ ft_strdup.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	int		i;
+
+	dup = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	i = 0;
+	if (!dup)
+		return (0);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
