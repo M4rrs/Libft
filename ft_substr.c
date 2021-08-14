e14bf388a1a902c8ec033e6dd1220ba42b057248
M4rrs <almasnorazman2803@outlook.com>
2021-08-14 16:48:58 +0800

    added

---
+++ ft_substr.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+char	*ft_substr(char const	*s, unsigned int	start, size_t	len)
+{
+	int				i;
+	char			*str;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup('\0'));
+	else if ((len + start) >= ft_strlen(s))
+		len = ft_strlen(s) - start;
+	str = (char *)malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		str[i] = str[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
