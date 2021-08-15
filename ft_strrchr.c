853f6887b3dd86486eeb9ebcd6bdec180c0a26d8
M4rrs <almasnorazman2803@outlook.com>
2021-08-15 20:47:47 +0800

    pain

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -4,13 +4,12 @@ char	*ft_strrchr(const char	*s, int	c)
 {
 	size_t					i;
 	unsigned char			*str;
-
-	str = (unsigned char *)s;
+	
 	i = ft_strlen((char *)s);
 	while (i >= 0)
 	{
-		if (str[i] == c)
-			return ((char *)&str[i]);
+		if (s[i] == c)
+			return ((char *)&s[i]);
 		i--;
 	}
 	return (NULL);
