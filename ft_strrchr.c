62a6c9365fca09975f97991609083663508c5fd3
M4rrs <almasnorazman2803@outlook.com>
2021-08-15 20:51:28 +0800

    cry

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -4,10 +4,10 @@ char	*ft_strrchr(const char	*s, int	c)
 {
 	size_t					i;
 
-	i = ft_strlen((char *)s);
+	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == (unsigned char *)c)
 			return ((char *)&s[i]);
 		i--;
 	}
