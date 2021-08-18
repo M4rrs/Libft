52bc6c4e15ae9e6d27f164ed6c0c480ca1be4986
M4rrs <almasnorazman2803@outlook.com>
2021-08-18 19:35:15 +0800

    fuck u

---
+++ ft_strjoin.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const	*s1, char const	*s2)
+{
+	char	*str;
+	char	*final;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (cha *r)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	final = str;
+	if (!str)
+		return (NULL);
+	while (*s1)
+		*str++ = *s1++;
+	while (*s2)
+		*str++ = *s2++;
+	*str = '\0';
+	return (final);
+}
