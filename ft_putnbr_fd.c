51d4fb77e5dceec5e25ace316931971f9d028133
M4rrs <almasnorazman2803@outlook.com>
2021-08-19 19:31:57 +0800

    change

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,7 +3,10 @@
 void	ft_putnbr_fd(int	n, int	fd)
 {
 	if (n >= 0 && n <= 9)
-		ft_putchar_fd(n + '0', fd);
+	{	
+		n += '0';
+		write(fd, &n, 1);
+	}
 	else if (n < 0 && n > -214783648)
 	{
 		ft_putchar_fd("-", fd);
