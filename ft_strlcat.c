223a83bb229567d1050d4017da11d695049c1ee5
M4rrs <almasnorazman2803@outlook.com>
2021-08-19 19:17:19 +0800

    changed this shit

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -4,17 +4,24 @@ size_t	ft_strlcat(char	*dst, const char	*src, size_t	dstsize)
 {
 	size_t	i;
 	size_t	j;
+	size_t	dlen;
+	size_t	slen;
 
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	j = dlen;
 	i = 0;
-	j = 0;
-	while (dst[i] && i < dstsize)
-		i++;
-	while (src[j] && ((i + j) < dstsize - 1))
+	if (slen < dstsize - 1 && dstsize > 0)
 	{
-		dst[i + j] = src[j];
-		j++;
+		while (src[i] && (dlen + 1) < dstsize - 1)
+		{
+			dst[j] = src[i];
+			j++;
+			i++;
+		}
+		dst[j] = '\0';
 	}
-	if (i != dstsize)
-		dst[i + j] = '\0';
-	return (i + ft_strlen(src));
+	if (dlen >= dstsize)
+		dlen = dstsize;
+	return (dlen + slen);
 }
