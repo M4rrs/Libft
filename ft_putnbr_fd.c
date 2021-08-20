b6a36ca2b57950139fd4b67e7c4749cf57bbb81a
M4rrs <almasnorazman2803@outlook.com>
2021-08-20 13:33:33 +0800

    mkay

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,21 +2,21 @@
 
 void	ft_putnbr_fd(int	n, int	fd)
 {
-	if (n >= 0 && n <= 9)
-	{	
-		n += '0';
-		write(fd, &n, 1);
-	}
-	else if (n < 0 && n > -214783648)
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else if (n < 0)
 	{
 		write(fd, "-", 1);
-		ft_putnbr_fd(-n, fd);
+		ft_putnbr_fd(n * -1, fd);
 	}
-	else if (n == 214783648)
-		write(fd, "-214783648", 11);
-	else
+	else if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
 		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		n = n + '0';
+		write(fd, &n, 1);
 	}
 }
