ba02357efe171aca5f8e1b88ac44fe40cc2ae291
M4rrs <almasnorazman2803@outlook.com>
2021-08-23 04:20:04 +0800

    honka honka

--- ft_tolower.c
+++ ft_tolower.c
@@ -5,29 +5,3 @@ int	ft_tolower(int	c)
 	return (c);
 }
 
-//#include <ctype.h>
-//#include <stdio.h>
-//int	main()
-//{
-//	char c = 'A';
-//	printf("%c\n", tolower(c));
-//	c = 'a';
-//	printf("%c\n", tolower(c));
-//	c = '9';
-//	printf("%c\n", tolower(c));
-//	c = '.';
-//	printf("%c\n", tolower(c));
-//	c = '\0';
-//	printf("%c\n\n", tolower(c));
-//	c = 'A';
-//	printf("%c\n", ft_tolower(c));
-//	c = 'a';
-//	printf("%c\n", ft_tolower(c));
-//	c = '9';
-//	printf("%c\n", ft_tolower(c));
-//	c = '.';
-//	printf("%c\n", ft_tolower(c));
-//	c = '\0';
-//	printf("%c\n", ft_tolower(c));
-//	return (0);
-//}
