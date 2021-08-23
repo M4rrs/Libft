d19008a954aacfa2b71d0ef15fd799940924e735
M4rrs <almasnorazman2803@outlook.com>
2021-08-23 15:53:40 +0800

    fixed

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -2,6 +2,17 @@
 
 void	ft_putendl_fd(char	*s, int	fd)
 {
-	write(fd, &s, ft_strlen(s));
-	write(fd, "\n", 1);
+	int	i;
+
+	i = 0;
+	if (s)
+	{
+		while (s[i])
+
+		{
+			write(fd, &s[i], 1);
+			i++;
+		}
+		write(fd, "\n", 1);
+	}
 }
