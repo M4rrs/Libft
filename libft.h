aee484d66284230ce3439e47530584e989b04f10
M4rrs <almasnorazman2803@outlook.com>
2021-08-27 04:21:47 +0800

    extra line u stupid isit

--- libft.h
+++ libft.h
@@ -11,7 +11,6 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-
 void	*ft_memset(void	*s, int	c, size_t	n);
 void	ft_bzero(void	*s, size_t	n);
 void	*ft_memcpy(void	*dest, const void	*src, size_t	n);
